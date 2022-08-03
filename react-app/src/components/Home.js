322c545f1907c544e17a30266f4a4860ac1cf4c1
m00nler <91968598+m00nler@users.noreply.github.com>
2022-08-03 13:42:56 +0300

    Added deleting method

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -3,6 +3,7 @@ import React, {Component, useState, useEffect} from "react";
 function Home()
 {
     const [values, setValues] = useState([]);
+    const forceUpdate = useForceUpdate();
     useEffect( () => {
         fetch(process.env["REACT_APP_API"] + "/api/department")
             .then((res) => res.json())
@@ -19,11 +20,31 @@ function Home()
                 <tr>
                     <th>{el.Id}</th>
                     <th>{el.Name}</th>
-                    <th>Edit/Delete</th>
+                    <th>
+                        <button className="edit">Edit</button>
+                        <button className="delete" onClick={async () => {
+                            const id = el.Id
+                            await deleteOnClick(el.Id);
+                            setValues(values.filter(el => el.Id != id))
+                        }
+                        }>Delete</button>
+                    </th>
                 </tr>
             )}
         </table>
     );
+}
+
+async function deleteOnClick(id)
+{
+   await fetch(process.env["REACT_APP_API"] + `/api/Department?id=${id}`, {
+        method:'DELETE'
+    })
+}
+
+function useForceUpdate(){
+    const [value, setValue] = useState(0);
+    return () => setValue(value => value + 1);
 }
 
 export default Home;
